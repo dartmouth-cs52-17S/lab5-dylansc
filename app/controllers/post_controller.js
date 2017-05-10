3e47e0552100b7c697e302ea7c855dd801c46663
dylansc <dylan.c.scandinaro.17@dartmouth.edu>
2017-05-10 17:47:02 -0400

    added sorting

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -26,7 +26,7 @@ export const createPost = (req, res) => {
 export const getPosts = (req, res) => {
   // res.send('posts should be returned\n');
   PostModel.find({})
-  // .sort('created_at')
+  .sort('created_at')
   .then((returnedPosts) => {
     console.log(returnedPosts);
     res.json(cleanPosts(returnedPosts));
